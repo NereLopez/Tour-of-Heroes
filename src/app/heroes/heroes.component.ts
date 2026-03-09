3d0ab92a7af2f28f2706b22b2f156ac1bf33e450
Nere Lopez <nlopez@pav-online.com>
2026-03-09 11:31:12 +0100

    nuevos heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,8 +12,7 @@ import { FormsModule } from '@angular/forms';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = { id: 1, name: 'Windstorm' };
-
+heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
